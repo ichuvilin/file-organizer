4994e2a6a73c30f5be393f0a6d5175c3bf0c69ce
Ivan Chuvilin <ivan.chuvilin@list.ru>
2026-09-21 21:48:20 +0500

    Create default rules

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import "fmt"
+
+var DefaultRules = map[string]string{
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".pdf":  "Documents",
+	".mp3":  "Music",
+}
+
 func main() {
+	folder := DefaultRules[".jpg"]
+	fmt.Println(folder)
 }
