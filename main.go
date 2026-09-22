14861d01f3906d7ede1cf3bbae5a0f82f7f6e918
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-22 08:16:56 +0500

    create FileOrganizer

--- main.go
+++ main.go
@@ -1,12 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 var DefaultRules = map[string]string{
 	".jpg":  "Images",
 	".jpeg": "Images",
 	".pdf":  "Documents",
 	".mp3":  "Music",
+}
+
+type FileOrganizer struct {
+	sourceDir      string
+	rulesMap       map[string]string
+	processedFiles int
+	logFile        *os.File
+}
+
+func NewFileOrganizer(sourceDir string) (*FileOrganizer, error) {
+	if sourceDir == "" {
+		return nil, errors.New("SourceDir not recognize")
+	}
+	info, err := os.Stat(sourceDir)
+	if err != nil {
+		return nil, err
+	}
+
+	if !info.IsDir() {
+		return nil, errors.New("sourceDir is not a dir")
+	}
+
+	return &FileOrganizer{sourceDir: sourceDir, rulesMap: DefaultRules}, nil
 }
 
 func main() {
