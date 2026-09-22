0085f17949bed3946c36c4dcfd871c6461492e78
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-22 08:58:50 +0500

    create base methods for work with logs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -34,6 +35,38 @@ func NewFileOrganizer(sourceDir string) (*FileOrganizer, error) {
 	}
 
 	return &FileOrganizer{sourceDir: sourceDir, rulesMap: DefaultRules}, nil
+}
+
+func (fo *FileOrganizer) initLog() error {
+	fileName := "organizer.log"
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+
+	log.SetOutput(file)
+	fo.logFile = file
+	return nil
+}
+
+func (fo *FileOrganizer) Close() error {
+	if fo.logFile == nil {
+		return nil
+	}
+
+	if err := fo.logFile.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fo *FileOrganizer) logSuccess(message string) {
+	log.Printf("[SUCCESS] %s\n", message)
+}
+
+func (fo *FileOrganizer) logError(message string) {
+	log.Printf("[ERROR] %s\n", message)
 }
 
 func main() {
